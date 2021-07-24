eccd3cf6abb7515590439e6fcbe5f28083c44214
oleg.krupko@onsolve.com <oleh.krupko@gmail.com>
2021-07-24 14:44:06 +0300

    docker restructured

--- scripts/docker/01-postgreSQL.sh
+++
@@ -1,3 +0,0 @@
-# install docker
-# curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
-# docker run --name homepage_postgresql -e POSTGRESQL_PASSWORD=bali4dead -d postgres
--- scripts/prepare5-YT.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install unclutter
-
-pathAutostart='/etc/xdg/lxsession/LXDE-pi/autostart'
-# add:
-echo "@unclutter -idle 0.1" >> $pathAutostart
-echo "@chromium-browser --kiosk --noerrdialogs --profile=Default --app=https://youtube.com/tv" >> $pathAutostart
